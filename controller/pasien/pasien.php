00df70af96ebbd1a80c76b52770236d76667bfc2
FATHUR RAHMAN ABDULLAH <63894938+fathurrahman0530@users.noreply.github.com>
2022-08-14 22:04:28 +0800

    solved create pasien

--- controller/pasien/pasien.php
+++ controller/pasien/pasien.php
@@ -12,8 +12,18 @@ try {
     $query_pasien = "INSERT INTO pasien (nama, hp, email) VALUES (?, ?, ?)";
     $query_user   = "INSERT INTO user (username, password, id_pasien) VALUES (?, ?, ?)";
     $conn->beginTransaction();
-    $stmt_pasien = $conn;
+    $stmt_pasien = $conn->prepare($query_pasien);
+    $stmt_user = $conn->prepare($query_user);
+    $stmt_pasien->execute([$nama, $hp, $email]);
+    $stmt_user->execute([$hp, sha1($hp), $conn->lastInsertId()]);
+    $conn->commit();
+    $respo['message'] = "Registrasi berhasil, silahkan login menggunakan nomor handphone yang didaftarkan sebagai username dan password";
   }
-} catch (\Throwable $th) {
-  //throw $th;
+} catch (Exception $e) {
+  if ($conn->inTransaction()) {
+    $conn->rollBack();
+  }
+  $response['message'] = "Gagal : " . $e->getMessage();
 }
+
+echo json_encode($response);
